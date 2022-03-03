f521fd50251745a1206fd9093f3a8755b8054222
FuerteTurtle <davegoodrich45+gitkraken@gmail.com>
2022-03-03 12:41:19 -0700

    added a score counter and made overflowing content scroll.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ $(window).ready(httpGetAsync(myURL, parseText));
 
 const interpret = XState.interpret;
 const createMachine = XState.createMachine;
+correctChoice = 0;
+total = 0;
+const guessesAllowed = 15;
 
 const gameMachine = createMachine({
     id: 'game',
@@ -26,29 +29,34 @@ const gameMachine = createMachine({
                 END: 'end'
             }
         },
-        end: { on: { RESTART: 'front' } }
+        end: { on: { RESTART: 'front' } },
+        guessesused: { on: { CONTINUE: 'front' }}
     }
 },
-    {
-        actions: {
-            updateBack: (context, event) => {
-                if (event.choice == currentCard.isBible) {
-                    // display right back
-                    $(".card-back").removeClass("back-incorrect");
-                    $(".card-back").addClass("back-correct");
-                    $("#back-title").html("CORRECT");
-                    $("#source").html(currentCard.source);
-                    $("#context").html(currentCard.context);
-                }
-                else {
-                    // display wrong back
-                    $(".card-back").removeClass("back-correct");
-                    $(".card-back").addClass("back-incorrect");
-                    $("#back-title").html("WRONG");
-                    $("#source").html(currentCard.source);
-                    $("#context").html(currentCard.context);
-                }
-            },
+{
+    actions: {
+        updateBack: (context, event) => {
+            total += 1;
+            if (event.choice == currentCard.isBible) {
+                // display right back
+                correctChoice += 1;
+                $(".card-back").removeClass("back-incorrect");
+                $(".card-back").addClass("back-correct");
+                $("#back-title").html("CORRECT");
+                $("#source").html(currentCard.source);
+                $("#context").html(currentCard.context);
+                $("#the-score").html(`${correctChoice}/${total}`);
+            }
+            else {
+                // display wrong back
+                $(".card-back").removeClass("back-correct");
+                $(".card-back").addClass("back-incorrect");
+                $("#back-title").html("WRONG");
+                $("#source").html(currentCard.source);
+                $("#context").html(currentCard.context);
+                $("#the-score").html(`${correctChoice}/${total}`);
+            }
+        },
             flipCard: () => {
                 if ($("#card-wrapper").hasClass("flip")) {
                     $("#card-wrapper").removeClass("flip");
@@ -57,15 +65,15 @@ const gameMachine = createMachine({
                     $("#card-wrapper").addClass("flip");
                 }
             },
-            updateFront: (context, event) => {
-                currentCard = event.myShuffledCards.next().value;
-                if(currentCard == undefined){
-                    console.log("end of game");
-                }
-                $(".card-front").html(currentCard.quote);
-            },
+                updateFront: (context, event) => {
+                    currentCard = event.myShuffledCards.next().value;
+                    if (currentCard == undefined) {
+                        console.log("end of game");
+                    }
+                    $(".front-content").html(currentCard.quote);
+                },
         }
-    });
+});
 const gameService = interpret(gameMachine);
 let shuffledCards = [];
 let currentCard = {};
@@ -74,7 +82,7 @@ function playGame(dataArray) {
     shuffledCards = shuffle(dataArray);
     gameService.start();
     currentCard = shuffledCards.next().value;
-    $(".card-front").html(currentCard.quote);
+    $(".front-content").html(currentCard.quote);
     $(".blasphemy").click(choice(false, currentCard));
     $(".bible").click(choice(true, currentCard));
     $(".card-back").click(next(shuffledCards));
@@ -82,7 +90,7 @@ function playGame(dataArray) {
 
 function choice(isCurrBible) {
     return function (e) {
-        gameService.send("CHOICE", { choice: isCurrBible});
+        gameService.send("CHOICE", { choice: isCurrBible });
     }
 }
 
