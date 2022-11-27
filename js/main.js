cdc9e2261d638d7c72761f4e1e4afb40d8f02fac
FuerteTurtle <davegoodrich45+gitkraken@gmail.com>
2022-11-26 22:16:26 -0700

    prettier and scroll overflow

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 class Datum {
-    constructor(quote = "", isBible = null, source = "", context = "") {
-        this.quote = quote;
-        this.isBible = isBible;
-        this.source = source;
-        this.context = context;
-    }
+  constructor(quote = "", isBible = null, source = "", context = "") {
+    this.quote = quote;
+    this.isBible = isBible;
+    this.source = source;
+    this.context = context;
+  }
 }
 $(window).ready(httpGetAsync(myURL, parseText));
 
@@ -14,96 +14,95 @@ correctChoice = 0;
 total = 0;
 const guessesAllowed = 15;
 
-const gameMachine = createMachine({
-    id: 'game',
-    initial: 'front',
+const gameMachine = createMachine(
+  {
+    id: "game",
+    initial: "front",
     states: {
-        front: {
-            on: {
-                CHOICE: { target: 'back', actions: ['updateBack', 'flipCard'] }
-            }
+      front: {
+        on: {
+          CHOICE: { target: "back", actions: ["updateBack", "flipCard"] },
         },
-        back: {
-            on: {
-                NEXT: { target: 'front', actions: ['updateFront', 'flipCard'] },
-                END: 'end'
-            }
+      },
+      back: {
+        on: {
+          NEXT: { target: "front", actions: ["updateFront", "flipCard"] },
+          END: "end",
         },
-        end: { on: { RESTART: 'front' } },
-        guessesused: { on: { CONTINUE: 'front' }}
-    }
-},
-{
+      },
+      end: { on: { RESTART: "front" } },
+      guessesused: { on: { CONTINUE: "front" } },
+    },
+  },
+  {
     actions: {
-        updateBack: (context, event) => {
-            total += 1;
-            if (event.choice == currentCard.isBible) {
-                // display right back
-                correctChoice += 1;
-                $(".card-back").removeClass("back-incorrect");
-                $(".card-back").addClass("back-correct");
-                $("#back-title").html("CORRECT");
-                $("#source").html(currentCard.source);
-                $("#context").html(currentCard.context);
-                $("#the-score").html(`${correctChoice}/${total}`);
-            }
-            else {
-                // display wrong back
-                $(".card-back").removeClass("back-correct");
-                $(".card-back").addClass("back-incorrect");
-                $("#back-title").html("WRONG");
-                $("#source").html(currentCard.source);
-                $("#context").html(currentCard.context);
-                $("#the-score").html(`${correctChoice}/${total}`);
-            }
-        },
-            flipCard: () => {
-                if ($("#card-wrapper").hasClass("flip")) {
-                    $("#card-wrapper").removeClass("flip");
-                }
-                else {
-                    $("#card-wrapper").addClass("flip");
-                }
-            },
-                updateFront: (context, event) => {
-                    currentCard = event.myShuffledCards.next().value;
-                    if (currentCard == undefined) {
-                        console.log("end of game");
-                    }
-                    $(".front-content").html(currentCard.quote);
-                },
+      updateBack: (context, event) => {
+        total += 1;
+        if (event.choice == currentCard.isBible) {
+          // display right back
+          correctChoice += 1;
+          $(".card-back").removeClass("back-incorrect");
+          $(".card-back").addClass("back-correct");
+          $("#back-title").html("CORRECT");
+          $("#source").html(currentCard.source);
+          $("#context").html(currentCard.context);
+          $("#the-score").html(`${correctChoice}/${total}`);
+        } else {
+          // display wrong back
+          $(".card-back").removeClass("back-correct");
+          $(".card-back").addClass("back-incorrect");
+          $("#back-title").html("WRONG");
+          $("#source").html(currentCard.source);
+          $("#context").html(currentCard.context);
+          $("#the-score").html(`${correctChoice}/${total}`);
         }
-});
+      },
+      flipCard: () => {
+        if ($("#card-wrapper").hasClass("flip")) {
+          $("#card-wrapper").removeClass("flip");
+        } else {
+          $("#card-wrapper").addClass("flip");
+        }
+      },
+      updateFront: (context, event) => {
+        currentCard = event.myShuffledCards.next().value;
+        if (currentCard == undefined) {
+          console.log("end of game");
+        }
+        $(".front-content").html(currentCard.quote);
+      },
+    },
+  }
+);
 const gameService = interpret(gameMachine);
 let shuffledCards = [];
 let currentCard = {};
 
 function playGame(dataArray) {
-    shuffledCards = shuffle(dataArray);
-    gameService.start();
-    currentCard = shuffledCards.next().value;
-    $(".front-content").html(currentCard.quote);
-    $(".blasphemy").click(choice(false, currentCard));
-    $(".bible").click(choice(true, currentCard));
-    $(".card-back").click(next(shuffledCards));
+  shuffledCards = shuffle(dataArray);
+  gameService.start();
+  currentCard = shuffledCards.next().value;
+  $(".front-content").html(currentCard.quote);
+  $(".blasphemy").click(choice(false, currentCard));
+  $(".bible").click(choice(true, currentCard));
+  $(".card-back").click(next(shuffledCards));
 }
 
 function choice(isCurrBible) {
-    return function (e) {
-        gameService.send("CHOICE", { choice: isCurrBible });
-    }
+  return function (e) {
+    gameService.send("CHOICE", { choice: isCurrBible });
+  };
 }
 
 function next(myShuffledCards) {
-    return function (e) {
-        gameService.send("NEXT", { myShuffledCards: myShuffledCards })
-    }
-
+  return function (e) {
+    gameService.send("NEXT", { myShuffledCards: myShuffledCards });
+  };
 }
 
 function* shuffle(array) {
-    var i = array.length;
-    while (i--) {
-        yield array.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
-    }
+  var i = array.length;
+  while (i--) {
+    yield array.splice(Math.floor(Math.random() * (i + 1)), 1)[0];
+  }
 }
--- js/sheets.js
+++ js/sheets.js
@@ -1,48 +1,51 @@
 const SPREADSHEET_ID = "1H-ZamZf4-GGu1TV2e8sqlv_VtnA0sWaFh92j_naraqQ";
 const SHEET_ID = 1;
-const API_KEY = "AIzaSyD17uZ-RUn-vYdJfJqqFhJw-Oiibmtlffc"
-const RANGE = "A2:D"
+const API_KEY = "AIzaSyD17uZ-RUn-vYdJfJqqFhJw-Oiibmtlffc";
+const RANGE = "A2:D";
 const myURL = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/Sheet${SHEET_ID}!${RANGE}?key=${API_KEY}`;
 // const devURL = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/Sheet${SHEET_ID}!${RANGE}?key=${PRIV_API_KEY}`
 
 function httpGetAsync(theUrl, callback) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function () {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(xmlHttp.responseText);
-    }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+      callback(xmlHttp.responseText);
+  };
+  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.send(null);
 }
 
-let dataArray = []
+let dataArray = [];
 
 function parseText(e) {
-    let response = JSON.parse(e);
-    response.values.forEach(line => {
-        dataArray.push(createDatum(line))
-    });
-    playGame(dataArray);
+  let response = JSON.parse(e);
+  response.values.forEach((line) => {
+    dataArray.push(createDatum(line));
+  });
+  playGame(dataArray);
 }
 
 //parse helper functions
 
-function checkIsBible(answer){
-    if(answer == "Yes"){
-        return true;
-    }
-    else return false;
+function checkIsBible(answer) {
+  if (answer == "Yes") {
+    return true;
+  } else return false;
 }
 
-function getContext(answer){
-    if(answer == "%" || answer == ""){
-        return ""
-    }
-    else{
-        return answer;
-    }
+function getContext(answer) {
+  if (answer == "%" || answer == "") {
+    return "";
+  } else {
+    return answer;
+  }
 }
 
-function createDatum(line){
-    return new Datum(line[0],checkIsBible(line[1]),line[2],getContext(line[3]))
+function createDatum(line) {
+  return new Datum(
+    line[0],
+    checkIsBible(line[1]),
+    line[2],
+    getContext(line[3])
+  );
 }
